1de6d142f68f037cf444b6dfaef6c5a56c171149
Maranje <fmaranje@princeton.edu>
2025-08-25 13:09:54 -0400

    adds basic player movement

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -3,3 +3,16 @@ extends CharacterBody2D
 @onready var player_sync_component: MultiplayerSynchronizer = $PlayerSyncComponent
 @onready var player_sync: MultiplayerSynchronizer = $PlayerSync
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var speed: int = 30000
+
+func _ready() -> void:
+	animated_sprite_2d.animation = "idle"
+	animated_sprite_2d.play()
+	
+func _process(delta: float) -> void:
+	velocity = player_sync_component.movement * delta * speed
+	if velocity.x < 0: animated_sprite_2d.animation = "left"
+	elif velocity.x > 0: animated_sprite_2d.animation = "right"
+	else: animated_sprite_2d.animation = "idle"
+	move_and_slide()
