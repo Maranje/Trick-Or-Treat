2e1cead258a90653f0f16b13d75b1bd66fb68ac6
FrankMaranje <frank.maranje@sti-tec.com>
2025-08-25 15:34:09 -0400

    fix player movement bug

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -4,14 +4,14 @@ extends CharacterBody2D
 @onready var player_sync: MultiplayerSynchronizer = $PlayerSync
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var speed: int = 30000
+var speed: int = 300
 
 func _ready() -> void:
 	animated_sprite_2d.animation = "idle"
 	animated_sprite_2d.play()
 	
-func _process(delta: float) -> void:
-	velocity = player_sync_component.movement * delta * speed
+func _process(_delta: float) -> void:
+	velocity = player_sync_component.movement * speed
 	if velocity.x < 0: animated_sprite_2d.animation = "left"
 	elif velocity.x > 0: animated_sprite_2d.animation = "right"
 	else: animated_sprite_2d.animation = "idle"
