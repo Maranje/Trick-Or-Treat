6480703e3fed943e0634b7596785bf47a9f2b927
Maranje <fmaranje@princeton.edu>
2025-08-28 21:04:11 -0400

    adds door trigger ID

--- Assets/Scripts/house_body.gd
+++ Assets/Scripts/house_body.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
 var player_at_door: bool = false
+var collider_index: int
 
 func _ready() -> void:
-	body_entered.connect(_player_entered)
+	body_shape_entered.connect(_player_entered)
 	body_exited.connect(_player_exited)
-	
-func _player_entered(body):
+
+func _player_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, local_shape_index: int):
+	collider_index = local_shape_index #get specific door trigger id
 	body.player_sync_component.toggle_at_door()
-	
+	print(collider_index)
+
 func _player_exited(body):
 	body.player_sync_component.toggle_at_door()
