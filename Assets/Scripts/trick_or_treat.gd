6aab719152512cfea475767384575458a1add138
Frank Maranje <fmaranje@alumni.princeton.edu>
2025-08-30 17:54:34 -0400

    updates costumes, tunes ToT sequence

--- Assets/Scripts/trick_or_treat.gd
+++ Assets/Scripts/trick_or_treat.gd
@@ -5,18 +5,20 @@ extends Node2D
 @onready var ding: AudioStreamPlayer2D = $Ding
 
 func _ready() -> void:
-	var tween = create_tween()
-	tween.set_ease(Tween.EASE_IN_OUT)
-	tween.set_trans(Tween.TRANS_BACK)
-	tween.tween_property(doorbell, "position", Vector2(0, -50), 0.5)
-	tween.parallel().tween_property(hand, "position", Vector2(-30, -100), 0.65)
-	tween.tween_callback(_doorbell_pressed)
+	var hand_tween = create_tween()
+	var doorbell_tween = create_tween()
+	hand_tween.set_ease(Tween.EASE_IN_OUT)
+	hand_tween.set_trans(Tween.TRANS_BACK)
+	doorbell_tween.set_ease(Tween.EASE_IN)
+	doorbell_tween.tween_property(doorbell, "position", Vector2(0, -50), 0.3)
+	hand_tween.tween_property(hand, "position", Vector2(-30, -100), 0.65)
+	doorbell_tween.tween_callback(_doorbell_pressed)
 
 func _doorbell_pressed():
 	ding.play()
 	var timer = Timer.new()
 	add_child(timer)
-	timer.wait_time = 0.5
+	timer.wait_time = 0.7
 	timer.one_shot = true
 	timer.timeout.connect(_remove_this_node)
 	timer.start()
